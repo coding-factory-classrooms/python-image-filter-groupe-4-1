705daa447c12554425468b9f09ab23711dfe05e1
lucas <lucasgauvain@gmail.com>
2020-11-25 14:01:15 +0100

    ajout de la méthode dilate_dir

--- filters/dilation.py
+++ filters/dilation.py
@@ -1,10 +1,32 @@
 import cv2
 import numpy as np
+import os
+import glob
+
+
 class Dilation:
 
     def dilate_file(self, filepath):
-        img = cv2.imread(filepath, 0)
-        kernel = np.ones((5, 5), np.uint8)
-        img_dilation = cv2.dilate(img, kernel, iterations=1)
-        cv2.imwrite(f'data/processed_images/test.jpg', img_dilation)
+        try:
+            img = cv2.imread(filepath)
+            file_name = os.path.basename(filepath)
+            kernel = np.ones((20, 20), np.uint8)
+            img_dilation = cv2.dilate(img, kernel, iterations=1)
+            output_file = f'data/output/{file_name}'
+            if output_file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                cv2.imwrite(f'data/output/{file_name}', img_dilation)
+        except Exception as e:
+            print(f'Please enter a correct file, \n error : {e}')
 
+    def dilate_dir(self, dir_path):
+        for filepath in glob.glob(f'{dir_path}*'):
+                try:
+                    img = cv2.imread(filepath)
+                    file_name = os.path.basename(filepath)
+                    kernel = np.ones((20, 20), np.uint8)
+                    img_dilation = cv2.dilate(img, kernel, iterations=1)
+                    output_file = f'data/output/{file_name}'
+                    if output_file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                        cv2.imwrite(f'data/output/{file_name}', img_dilation)
+                except Exception as e:
+                    print(f'Please make sure your directory is correct, \n error : {e}')
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from filters.blur import Blur
 from filters.black_and_white import BlackAndWhite
 from filters.dilation import Dilation
-g
+
+dilate = Dilation()
+
+dilate.dilate_dir('data/images/')
