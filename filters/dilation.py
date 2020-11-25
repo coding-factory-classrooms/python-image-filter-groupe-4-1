78202b57d504a004f4d962007689952a8dc00762
lucas <lucasgauvain@gmail.com>
2020-11-25 16:55:17 +0100

    drying the code

--- filters/dilation.py
+++ filters/dilation.py
@@ -2,31 +2,22 @@ import cv2
 import numpy as np
 import os
 import glob
+from filters.read_write_file import ReadWrite
+
 
 
 class Dilation:
+    output_file = "data/output/"
 
-    def dilate_file(self, filepath):
-        try:
-            img = cv2.imread(filepath)
-            file_name = os.path.basename(filepath)
-            kernel = np.ones((20, 20), np.uint8)
-            img_dilation = cv2.dilate(img, kernel, iterations=1)
-            output_file = f'data/output/{file_name}'
-            if output_file.lower().endswith(('.png', '.jpg', '.jpeg')):
-                cv2.imwrite(f'data/output/{file_name}', img_dilation)
-        except Exception as e:
-            print(f'Please enter a correct file, \n error : {e}')
-
-    def dilate_dir(self, dir_path):
-        for filepath in glob.glob(f'{dir_path}*'):
-                try:
-                    img = cv2.imread(filepath)
-                    file_name = os.path.basename(filepath)
-                    kernel = np.ones((20, 20), np.uint8)
-                    img_dilation = cv2.dilate(img, kernel, iterations=1)
-                    output_file = f'data/output/{file_name}'
-                    if output_file.lower().endswith(('.png', '.jpg', '.jpeg')):
-                        cv2.imwrite(f'data/output/{file_name}', img_dilation)
-                except Exception as e:
-                    print(f'Please make sure your directory is correct, \n error : {e}')
+    def dilate_file(self, filepath, filter):
+        read_write = ReadWrite()
+        img = read_write.read_file(filepath)
+        kernel = np.ones((20, 20), np.uint8)
+        output_image = ""
+        if filter == 'dilate':
+            output_image = cv2.dilate(img, kernel, iterations=1)
+        elif filter == 'blur':
+            output_image = cv2.GaussianBlur(img, (7, 7), 0)
+        elif filter == 'black_and_white':
+            output_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        read_write.write_file(filepath, output_image)
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from filters.blur import Blur
 from filters.black_and_white import BlackAndWhite
 from filters.dilation import Dilation
+import glob
+import os
+
 
 dilate = Dilation()
 
-dilate.dilate_dir('data/images/')
+
+dir_path = "data/images/"
+
+for filepath in glob.glob(f'{dir_path}*'):
+    try:
+        if filepath.lower().endswith(('.png', '.jpg', '.jpeg')):
+            dilate.dilate_file(filepath, 'dilate')
+    except Exception as e:
+        print(f'Please make sure your directory is correct, \nerror : {e}')
