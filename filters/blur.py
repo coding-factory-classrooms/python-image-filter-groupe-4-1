f8c9d630c51c093a5dee7a824b5e74b61587bed9
lucas <lucasgauvain@gmail.com>
2020-11-27 11:30:35 +0100

    ajout du la verif blur impair

--- filters/blur.py
+++ filters/blur.py
@@ -11,6 +11,8 @@ class Blur:
         :param img: The image path
         :return:
         """
+        if intensity % 2 == False:
+            intensity += 1
         log = Logger()
         log.log(" Applying a blur filter...")
         # Blur the image with gaussian filter
